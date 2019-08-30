981bfe8c107c222045e006e47c2e5e5ec67393ed
SageSanyue <562441461@qq.com>
2019-08-30 09:49:09 +0800

    pros和¶子传值

---
+++ src/TodoList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <h1>Todo-list</h1>
+    <input v-model="inputValue" type="text">
+    <button @click="handleSubmit">提交</button>
+    <todo-item 
+      v-for="(item, index) of list" 
+      :key="index"
+      :content="item"
+      :index="index"
+      @delete="handleDelete"
+    >
+    </todo-item>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'TodoList',
+  components: {
+    'todo-item': TodoItem
+  },
+  data() {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit() {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete(index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li @click="handleClick">{{content}}</li>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['content', 'index'],
+  methods: {
+    handleClick() {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+li {
+  color: yellowgreen;
+}
+</style>
